79f803560e71a1054c2cc72707c2614b9e154220
S.Mohammad Hosseininejad <smohammadhnd@gmail.com>
2022-07-20 19:01:22 +0430

    fresh new nuxt3 installation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: [
-    '~/assets/styles/normalize.css',
     '~/assets/styles/global.scss',
     '~/assets/styles/variables.scss',
   ],
