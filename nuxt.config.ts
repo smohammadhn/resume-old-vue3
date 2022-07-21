e3b89eaba733033f84b863c7138161696691a089
S.Mohammad Hosseininejad <smohammadhnd@gmail.com>
2022-07-21 12:53:32 +0430

    feat: add css styles to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,9 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: [
-    '~/assets/styles/global.scss',
+    '~/assets/styles/reset.css',
+    '~/assets/styles/fonts.css',
     '~/assets/styles/variables.scss',
+    '~/assets/styles/global.css',
   ],
 })
